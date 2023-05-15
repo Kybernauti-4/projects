75660a7180e28b309a2f4c4672a3974468e97a03
vasek9876 <76277379+vasek9876@users.noreply.github.com>
2023-05-15 16:18:31 +0200

    Update Arduino_SegmentDisplay.ino

--- maturita/codes/Arduino_SegmentDIsplay/Arduino_SegmentDisplay.ino
+++ maturita/codes/Arduino_SegmentDIsplay/Arduino_SegmentDisplay.ino
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 
-const int arr_position[] = {2, 3, 4, 5}; // Definování pinů pro pozici (1, 2, 3, 4) - rezistory
+const int arr_position[] = {2, 3, 4, 5}; // Definování pinů pro pozici (1, 2, 3, 4) - přidat rezistory
 const int arr_segment[] = {6, 7, 8, 9, 10, 11, 12, 13}; // Definování pinů pro konkrétní "pixel" (a, b, c, d, e, f, g, h)
 
+// data 
 const byte data[5][8] = {
 //(a, b, c, d, e, f, g, h)
   {1, 1, 1, 1, 0, 1, 1, 0}, // A
@@ -13,25 +14,28 @@ const byte data[5][8] = {
 };
 
 void setup() {
+  // nastaví output a low pro 1,2,3,4
   for (int i = 0; i < 4; i++) {
     pinMode(arr_position[i], OUTPUT);
     digitalWrite(arr_position[i], LOW);
   }
-
+  // nastaví output pro a, b, c, d, e, f, g, h
   for (int i = 0; i < 8; i++) {
     pinMode(arr_segment[i], OUTPUT);
   }
 }
 
 void loop() {
+  // opakuji pro každý segment
   for (int i = 0; i < 4; i++) {
+    // *nastaví* data konkrétního segmentu
     for (int n = 0; n < 8; n++) {
-      digitalWrite(arr_segment[n], not data[i][n]);
+      digitalWrite(arr_segment[n], not data[i][n]); // not protože data jsou reverzní
     }
-    digitalWrite(arr_position[i], HIGH);
-
+    
+    // *zobrazím* data konkrétního segmentu
+    digitalWrite(arr_position[i], HIGH); 
     delay(1);
-
     digitalWrite(arr_position[i], LOW);
   }
 } 
